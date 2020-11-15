6f1ff606b510dd38cefb272474948e59bd21634b
Calvin Cox <calvinkcox@gmail.com>
2020-11-15 12:10:58 -0600

    Loading the rooms and showing on the page

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,27 @@
-import React, { FC } from "react";
+import React, { FC, useCallback } from "react";
+import useRemoteResource from "../hooks/use-remote-resource";
+import { Room } from "../models/domain-objects";
+import NavLink from "./NavLink";
+
+const emptyRooms = [] as Room[];
 
 interface Props { }
 
 const Navigation: FC<Props> = () => {
+  const fetchRooms: () => Promise<Room[]> = useCallback(() => {
+    return fetch("/rooms").then((res) => res.json());
+  }, []);
+  const [error, isLoading, rooms] = useRemoteResource(fetchRooms, emptyRooms);
+
+  if (error) return <div>Oh no! It blew up...</div>
+  if (isLoading) return <div>Loading chat rooms...</div>
 
   return (
-    <div>get navigation sections and put here</div>
+    <ul>
+      {rooms.map((r) => (
+        <NavLink roomId={r.roomId} name={r.name} />
+      ))}
+    </ul>
   );
 };
 
