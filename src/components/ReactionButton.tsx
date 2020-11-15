d1f5ba4f098e628115a9e02e165e6f710f4184e3
Calvin Cox <calvinkcox@gmail.com>
2020-11-15 12:34:57 -0600

    Simple reaction button

---
+++ src/components/ReactionButton.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+import { Reaction } from "../models/domain-objects";
+
+interface Props extends Reaction { }
+
+let ReactionButton: FC<Props> = ({ emoji, count }) => {
+  return (
+    <button className="reaction-button">
+      <span>{emoji}</span>
+      <span className="count">{count}</span>
+    </button>
+  );
+};
+
+export default ReactionButton;
